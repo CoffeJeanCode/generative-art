742abc9ad0818626db318f5406af15ed7cb0bcca
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2023-12-08 19:07:58 -0500

    Add 7.Fractals

---
+++ lessons/7-fractals/sketch.js
@@ -0,0 +1,78 @@
+let maxLevels = 5;
+let numChildren = 2;
+let _trunk;
+let noiseVal = 0.0;
+let noiseIncrement = 0.01;
+
+class Branch {
+  constructor(lev, idx, ex, why) {
+    this.x = 0;
+    this.y = 0;
+    this.endx = 0;
+    this.endy = 0;
+    this.level = lev;
+    this.idx = idx;
+    this.children = [];
+
+    this.strokeW = (1 - 1 / this.level) * 25;
+    this.alph = 255 / this.level;
+    this.len = (1 / this.level) * random(100);
+    this.rot = random(360);
+    this.lenChange = random(10) - 10;
+    this.rotChange = random(10) - 10;
+
+    this.update(ex, why);
+
+    if (this.level < maxLevels) {
+      for (let i = 0; i < numChildren; i++) {
+        this.children.push(new Branch(this.level + 1, i, this.endx, this.endy));
+      }
+    }
+  }
+
+  draw() {
+    strokeWeight(maxLevels - this.level + 1);
+    line(this.x, this.y, this.endx, this.endy);
+    ellipse(this.x, this.y, 5, 5);
+    this.children.forEach((child) => {
+      child.draw();
+    });
+  }
+
+  update(ex, why) {
+    this.x = ex;
+    this.y = why;
+
+    this.rot += this.rotChange;
+    this.rot = this.rot > 360 ? (this.root < 0 ? 360 : 0) : this.rot;
+
+    this.len -= (this.lenChange * 1) / this.len;
+    this.lenChange *= this.len < 0 ? -1 : this.len > 200 ? -1 : 1;
+
+    let radian = radians(this.rot);
+    this.endx = this.x + this.len * cos(radian);
+    this.endy = this.y + this.len * sin(radian);
+
+    this.children.forEach((child) => child.update(this.endx, this.endy));
+  }
+}
+
+function setup() {
+  createCanvas(600, 600);
+  background(255);
+  noFill();
+  smooth();
+
+  if (frameCount > 100) {
+    noLoop();
+  }
+
+  _trunk = new Branch(1, 0, width / 2, 50);
+}
+
+function draw() {
+  noiseVal += noiseIncrement;
+  background(255);
+  _trunk.update(width / 2, height / 2);
+  _trunk.draw(width / 2, height / 2);
+}
