3259cb57b3f1d4367346bf53d2b393a86955a475
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2023-12-08 19:07:39 -0500

    change  5.1. normal noise to custom noise

--- lessons/5-dimensions/1-2d/index.js
+++ lessons/5-dimensions/1-2d/index.js
@@ -14,9 +14,46 @@ function setup() {
 
     for (let x = 0; x <= width; x += 5) {
       xnoise += 0.1;
-      drawLine(x, y, noise(xnoise, ynoise));
+      drawLine(x, y, customNoise(xnoise, ynoise));
     }
   }
+}
+
+function customNoise(x, y) {
+  let x0 = Math.floor(x);
+  let y0 = Math.floor(y);
+  let x1 = x0 + 1;
+  let y1 = y0 + 1;
+
+  let sx = x - x0;
+  let sy = y - y0;
+
+  let n0, n1, ix0, ix1, value;
+
+  ix0 = gradientDotProduct(x0, y0, x, y);
+  ix1 = gradientDotProduct(x1, y0, x, y);
+  n0 = lerp(ix0, ix1, sx);
+
+  ix0 = gradientDotProduct(x0, y1, x, y);
+  ix1 = gradientDotProduct(x1, y1, x, y);
+  n1 = lerp(ix0, ix1, sx);
+
+  value = lerp(n0, n1, sy);
+  return value;
+}
+
+function gradientDotProduct(ix, iy, x, y) {
+  let dx = x - ix;
+  let dy = y - iy;
+
+  let random =
+    2920 *
+    Math.sin(ix * 21942 + iy * 171324 + 8912) *
+    Math.cos(ix * 23157 * iy * 217832 + 9758);
+  let gradient = { x: Math.cos(random), y: Math.sin(random) };
+
+  let distanceVec = { x: dx, y: dy };
+  return gradient.x * distanceVec.x + gradient.y * distanceVec.y;
 }
 
 function drawSquare(x, y, noiseFactor) {
@@ -28,7 +65,11 @@ function drawLine(x, y, noiseFactor) {
   push();
   translate(x, y);
   rotate(noiseFactor * radians(360));
-  stroke(0, 150);
+
+  // Mapear el valor del ruido a una paleta de colores
+  colorMode(HSB, 100);
+  strokeWeight(map(noiseFactor, 0, 1, 0.25, 1)); // Mapear el grosor del trazo según el ruido
+  stroke(21); // Establecer el color mapeado como color de trazo
   line(0, 0, 20, 0);
   pop();
 }
