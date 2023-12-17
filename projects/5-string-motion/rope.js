67488f817fb4a10140f3f2a2882b95a973628fbd
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2023-12-17 15:07:48 -0500

    refactor code and add time hover motion

--- projects/5-string-motion/rope.js
+++ projects/5-string-motion/rope.js
@@ -2,117 +2,112 @@ class Rope {
   constructor(from, to, divisions) {
     this.from = from;
     this.to = to;
+
     this.divisions = divisions;
-    const deltaLine = p5.Vector.sub(this.to, this.from);
-    const diffSplit = deltaLine.div(this.divisions);
-    this.splits = floatRange(0, this.divisions, 1).map((i) => {
-      const x = this.from.x + diffSplit.x * i;
-      const y = this.from.y + diffSplit.y * i;
+    this.segments = this.setSegments();
+    this.originalSegements = [...this.segments];
+
+    this.hovered = false; 
+    this.hoverStartTime = 0; 
+    this.decayRate = 0.002;
+  }
+
+  setSegments() {
+    const lineLength = p5.Vector.sub(this.to, this.from);
+    const spacing = lineLength.div(this.divisions);
+
+    return range(0, this.divisions, 1).map((i) => {
+      const x = this.from.x + spacing.x * i;
+      const y = this.from.y + spacing.y * i;
       return createVector(x, y);
     });
-    this.originalList = [...this.splits];
   }
 
   draw() {
     beginShape();
 
-    curveVertex(this.splits[0].x, this.splits[0].y);
+    curveVertex(this.segments[0].x, this.segments[0].y);
 
-    this.splits.forEach((split) => {
-      const { x, y } = split;
-      curveVertex(x, y);
+    this.segments.forEach((segment) => {
+      curveVertex(segment.x, segment.y);
     });
 
     curveVertex(
-      this.splits[this.splits.length - 1].x,
-      this.splits[this.splits.length - 1].y
+      this.segments[this.segments.length - 1].x,
+      this.segments[this.segments.length - 1].y
     );
 
     endShape();
   }
 
-  update() {
-    const fallOff = sin(frameCount / 2);
-    const offsetList = floatRange(0, this.divisions, 1).map((offset) =>
+  update(x, y, radius) {
+    const currentTime = Date.now();
+
+    if (this.isHovered(x, y, radius)) {
+      this.handleHover(currentTime);
+    } else {
+      this.handleNoHover();
+    }
+  }
+
+  handleHover(currentTime) {
+    if (!this.hovered) {
+      this.hovered = true;
+      this.hoverStartTime = currentTime;
+    }
+
+    const elapsedTime = currentTime - this.hoverStartTime;
+
+    const decay = Math.exp(-this.decayRate * elapsedTime);
+
+    const fallOff = Math.sin(frameCount / 2) * decay;
+
+    const offsetVectors = range(0, this.divisions, 1).map((offset) =>
       createVector(
         mirrorInterpolation(sinusoidalInterpolation)(
-          offset / this.splits.length,
+          offset / this.segments.length,
           0,
           1
         ),
         0
       )
     );
-    const modifiedList = offsetVectorList(this.splits, offsetList, fallOff);
 
-    if (this.isHovered()) {
-      this.splits = modifiedList;
-    } else {
-      this.splits = this.originalList;
-    }
+    const modifiedList = offsetVectorList(
+      this.segments,
+      offsetVectors,
+      fallOff
+    );
+
+    this.segments = modifiedList;
   }
 
-  isHovered() {
-    const mouseXPos = mouseX - width / 2;
-    const mouseYPos = mouseY - height / 2;
+  handleNoHover() {
+    this.hovered = false;
+    this.segments = this.originalSegements;
+  }
 
-    let isInside = false;
+  isHovered(x, y, radius = 10) {
+    const mouseXPos = x - width / 2;
+    const mouseYPos = y - height / 2;
 
-    for (let i = 0; i < this.splits.length - 1; i++) {
-      const p1 = this.splits[i];
-      const p2 = this.splits[i + 1];
+    let isHovering = false;
+
+    for (let i = 0; i < this.segments.length - 1; i++) {
+      const p1 = this.segments[i];
+      const p2 = this.segments[i + 1];
 
       const d1 = dist(mouseXPos, mouseYPos, p1.x, p1.y);
       const d2 = dist(mouseXPos, mouseYPos, p2.x, p2.y);
 
-      const d = this.distToSegment(
-        mouseXPos,
-        mouseYPos,
-        p1.x,
-        p1.y,
-        p2.x,
-        p2.y
-      );
+      const d = distToSegment(mouseXPos, mouseYPos, p1.x, p1.y, p2.x, p2.y);
 
-      if (d < 10 || d1 < 10 || d2 < 10) {
-        // Define aquí un umbral para la distancia
-        isInside = true;
+      if (d < radius || d1 < radius || d2 < radius) {
+        isHovering = true;
         break;
       }
     }
 
-    return isInside;
-  }
-
-  distToSegment(x, y, x1, y1, x2, y2) {
-    const A = x - x1;
-    const B = y - y1;
-    const C = x2 - x1;
-    const D = y2 - y1;
-
-    const dot = A * C + B * D;
-    const lenSq = C * C + D * D;
-    let param = -1;
-
-    if (lenSq !== 0) {
-      param = dot / lenSq;
-    }
-
-    let xx, yy;
-
-    if (param < 0) {
-      xx = x1;
-      yy = y1;
-    } else if (param > 1) {
-      xx = x2;
-      yy = y2;
-    } else {
-      xx = x1 + param * C;
-      yy = y1 + param * D;
-    }
-
-    const dx = x - xx;
-    const dy = y - yy;
-    return sqrt(dx * dx + dy * dy);
+    return isHovering;
   }
 }
--- projects/5-string-motion/sketch.js
+++ projects/5-string-motion/sketch.js
@@ -1,12 +1,12 @@
 const ropes = [];
-let rope;
-let tap;
+let mouse;
+
 function setup() {
   createCanvas(600, 600);
-  tap = { pos: createVector(0, 0), r: 20 };
-
+  mouse = { pos: createVector(0, 0), rad: 20 };
+  noCursor();
   for (let i = 0; i < 6; i++) {
-    const x = i * 20;
+    const x = i * 20 - 50;
     const y = 200;
     ropes.push(new Rope(createVector(x, y), createVector(x, -y), 50));
   }
@@ -15,15 +15,16 @@ function setup() {
 function draw() {
   background(21);
   translate(width / 2, height / 2);
-  stroke(255);
+
+  mouse.pos.set(mouseX - width / 2, 0);
+
   fill(255);
+  stroke(255);
+  circle(mouse.pos.x, mouse.pos.y, mouse.rad * 2);
 
-  tap.pos.set(mouseX - width / 2, 0);
-  circle(tap.pos.x, tap.pos.y, tap.r);
-
-  noFill();
   ropes.forEach((rope) => {
+    noFill();
     rope.draw();
-    rope.update();
+    rope.update(mouseX, mouseY, mouse.rad);
   });
 }
