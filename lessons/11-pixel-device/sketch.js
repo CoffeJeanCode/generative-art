30e41840717d0ec4739814d12c104c2aaf834fb2
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2024-02-09 05:35:05 -0500

    lesson 11: device pixel

---
+++ lessons/11-pixel-device/sketch.js
@@ -0,0 +1,50 @@
+const cnvWidth = 600;
+const cnvHeight = 600;
+const resizeCnv = resized(cnvHeight, cnvHeight);
+
+function resized(width, height) {
+  return function (pixelRatio) {
+    return [width * pixelRatio, height * pixelRatio];
+  };
+}
+
+var s1 = function (sketch) {
+  sketch.setup = function () {
+    const [width, height] = resizeCnv(devicePixelRatio);
+    let canvas1 = sketch.createCanvas(width, height, sketch.WEBGL);
+    canvas1.position(0, 0);
+  };
+  sketch.draw = function () {
+    //for canvas 1
+    sketch.background(100);
+    sketch.rotateX(sketch.frameCount * 0.01);
+    sketch.rotateZ(sketch.frameCount * 0.01);
+    sketch.cone(30, 50);
+  };
+
+  sketch.windowResized = function () {
+    const [width, height] = resizeCnv(devicePixelRatio);
+
+    resizeCanvas(width, height);
+  };
+};
+
+// create a new instance of p5 and pass in the function for sketch 1
+new p5(s1);
+
+var s2 = function (sketch) {
+  sketch.setup = function () {
+    let canvas2 = sketch.createCanvas(cnvWidth, cnvHeight, sketch.WEBGL);
+    canvas2.position(cnvWidth, 0);
+  };
+  sketch.draw = function () {
+    //for canvas 2
+    sketch.background(100);
+    sketch.rotateX(sketch.frameCount * 0.01);
+    sketch.rotateZ(sketch.frameCount * 0.02);
+    sketch.cone(30, 50);
+  };
+};
+
+// create the second instance of p5 and pass in the function for sketch 2
+new p5(s2);
